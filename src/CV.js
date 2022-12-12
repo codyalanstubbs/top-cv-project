9809120c0f2ba6ec59f2a5a2b86582e4faa2040d
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-12-12 15:52:27 -0500

    Create the overall CV component

---
+++ src/CV.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import './assets/css/App.css';
+
+class CV extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form className="cv">
+  
+      </form>
+    );
+  }
+}
+
+export default CV;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
-import App from './App';
+import CV from './CV';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CV />
   </React.StrictMode>
 );
