3848073f7b1729a5513c078271bb3e216d12b5c2
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-12-13 10:03:31 -0500

    Reformat Experience class

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,30 +9,30 @@ class Experience extends Component {
         return (
             <div className="experience">
                 <h4 className="role">Coolness Research Scientist</h4>
-                    <div className="date-range">
-                        <p className="date start">May 2018</p>
-                        <p>to</p>
-                        <p className="date end">Sep 2021</p>
-                    </div>
-                    <em className="company">Coolness Insights, The Coolness Company</em>
-                    <ul className="responsibilities">
-                        <li className="responsibility">
-                            Managed, trained, and maintained the internal Coolnes panel for cross-functional and global research
-                        </li>
-                        <li className="responsibility">
-                            Prioritized Coolness panel’s projects weekly to balance research and Coolness screening workloads
-                        </li>
-                        <li className="responsibility">
-                            Tailored analyses and communications of results based on clients’ needs and background
-                        </li>
-                        <li className="responsibility">
-                            Built strong, collaborative relationships with cross-functional innovation peers quickly and effectively
-                        </li>
-                        <li className="add responsibility">
-                            + Add Another Responsibility
-                        </li>
-                    </ul>
-                    <div className="remove experience">- Remove Experience ↑</div>
+                <div className="date-range">
+                    <p className="date start">May 2018</p>
+                    <p>to</p>
+                    <p className="date end">Sep 2021</p>
+                </div>
+                <em className="company">Coolness Insights, The Coolness Company</em>
+                <ul className="responsibilities">
+                    <li className="responsibility">
+                        Managed, trained, and maintained the internal Coolnes panel for cross-functional and global research
+                    </li>
+                    <li className="responsibility">
+                        Prioritized Coolness panel’s projects weekly to balance research and Coolness screening workloads
+                    </li>
+                    <li className="responsibility">
+                        Tailored analyses and communications of results based on clients’ needs and background
+                    </li>
+                    <li className="responsibility">
+                        Built strong, collaborative relationships with cross-functional innovation peers quickly and effectively
+                    </li>
+                    <li className="add responsibility">
+                        + Add Another Responsibility
+                    </li>
+                </ul>
+                <div className="remove experience">- Remove Experience ↑</div>
             </div>
         );
     }
