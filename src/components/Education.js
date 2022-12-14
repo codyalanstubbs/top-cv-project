e8871c2a05418d659afae72d30a9dfc127c25364
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-12-14 14:39:24 -0500

    Allow user to add educations
    Place AddAnotherBtn in Education component.
    Add state to Education component to enable button to add educations.
    Restructure the JSX to use map to dynamically build the educations
    based on the expriences array in state.

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,24 +1,51 @@
 import { Component } from 'react';
 import DateRange from './DateRange';
 import EditableText from './EditableText';
+import AddAnotherBtn from './AddAnotherBtn';
+import uniqid from 'uniqid';
 
 class Education extends Component {
     constructor(props) {
         super(props);
+
+        this.addAnother = this.addAnother.bind(this);
+
+        this.state = {
+            educations  : [{
+                id      : uniqid(),
+            }],
+        };
+    }
+
+    addAnother() {
+        this.setState({
+            educations  : this.state.educations.concat({id  : uniqid()})
+        });
     }
 
     render() {
         return (
-            <div className="edu">
-                <div className="institution-info">
-                    <EditableText element="h4" textClass="institution"/>
-                    <p>,&nbsp;</p>
-                    <EditableText element="p" textClass="location"/>
-                </div>
-                <DateRange />
-                <EditableText element="em" textClass="degree"/>
-                <div className='remove-container'>
-                    <div className="remove experience">- Remove Education ↑</div>
+            <div>
+                { // Create the education elements based on the educations array in state
+                    this.state.educations.map((edu) => {
+                        return (
+                            <div key={edu.id} id={edu.id} className="edu">
+                                <div className="institution-info">
+                                    <EditableText element="h4" textClass="institution"/>
+                                    <p>,&nbsp;</p>
+                                    <EditableText element="p" textClass="location"/>
+                                </div>
+                                <DateRange />
+                                <EditableText element="em" textClass="degree"/>
+                                <div className='remove-container'>
+                                    <div className="remove education">- Remove Education ↑</div>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+                <div className="button-container" onClick={this.addAnother}>
+                    <AddAnotherBtn sectionName="education"  />
                 </div>
             </div>
         );
