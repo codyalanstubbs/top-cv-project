79deb197a8588ef1268cad07ed89d1ad9e40595b
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-12-15 11:17:32 -0500

    Separate out Section styling

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Content from './Content';
 import EditableText from './EditableText';
+import '../assets/css/Section.css';
 
 class Section extends Component {
     constructor(props) {
