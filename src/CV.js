316ff108831126963d6f50c8d6208976bf45ac8d
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-12-19 11:18:50 -0500

    Make CV class component a function component

--- src/CV.js
+++ src/CV.js
@@ -1,22 +1,15 @@
-import { Component } from 'react';
 import './assets/css/CV.css';
 import Section from './components/Section';
 
-class CV extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <form className="cv">
-        <Section sectionName="header"></Section>
-        <Section sectionName="summary"></Section>
-        <Section sectionName="education"></Section>
-        <Section sectionName="experience"></Section>
-      </form>
-    );
-  }
-}
+const CV = () => {
+  return (
+    <form className="cv">
+      <Section sectionName="header"></Section>
+      <Section sectionName="summary"></Section>
+      <Section sectionName="education"></Section>
+      <Section sectionName="experience"></Section>
+    </form>
+  );
+};
 
 export default CV;
