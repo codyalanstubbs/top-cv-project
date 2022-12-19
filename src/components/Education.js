9541d801b2f3d9e8bd9c69ca6985c78d2cbfa69b
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-12-19 13:03:58 -0500

    Make Education class compoonent a functional component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,69 +1,58 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import DateRange from './DateRange';
 import EditableText from './EditableText';
 import AddAnotherBtn from './AddAnotherBtn';
 import uniqid from 'uniqid';
 import '../assets/css/Education.css';
 
-class Education extends Component {
-    constructor(props) {
-        super(props);
+const Education = () => {
+    const [educations, setEducations] = useState([]);
 
-        this.add = this.add.bind(this); 
-        this.remove = this.remove.bind(this); 
-        
-        this.state = {
-            educations : []
-        };
-    }
+    const add = () => {
+        setEducations(
+            educations.concat({id  : uniqid()})
+        );
+    };
 
-    add() {
-        this.setState({
-            educations : this.state.educations.concat({id  : uniqid()})
-        });
-    }
-
-    remove(event) {
-        this.setState({
-            educations: this.state.educations.filter((edu) => {
+    const remove = (event) => {
+        setEducations(
+            educations.filter((edu) => {
                 return edu.id !== event.target.id;
-            }),
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                { // Create the education elements based on the educations array in state
-                    this.state.educations.map((edu) => {
-                        return (
-                            <div key={edu.id} id={edu.id} className="edu">
-                                <div className="institution-info">
-                                    <EditableText element="h4" textClass="institution"/>
-                                    <p>,&nbsp;</p>
-                                    <EditableText element="p" textClass="location"/>
-                                </div>
-                                <DateRange />
-                                <EditableText element="em" textClass="degree"/>
-                                <div className='remove-container'>
-                                    <div 
-                                    id={edu.id} 
-                                    className="remove education"
-                                    onClick={this.remove}
-                                    >
-                                        - Remove Education ↑
-                                    </div>
+            })
+        );
+    };
+    
+    return (
+        <div>
+            { // Create the education elements based on the educations array in state
+               educations.map((edu) => {
+                    return (
+                        <div key={edu.id} id={edu.id} className="edu">
+                            <div className="institution-info">
+                                <EditableText element="h4" textClass="institution"/>
+                                <p>,&nbsp;</p>
+                                <EditableText element="p" textClass="location"/>
+                            </div>
+                            <DateRange />
+                            <EditableText element="em" textClass="degree"/>
+                            <div className='remove-container'>
+                                <div 
+                                id={edu.id} 
+                                className="remove education"
+                                onClick={remove}
+                                >
+                                    - Remove Education ↑
                                 </div>
                             </div>
-                        );
-                    })
-                }
-                <div className="button-container" onClick={this.add}>
-                    <AddAnotherBtn sectionName="education"  />
-                </div>
+                        </div>
+                    );
+                })
+            }
+            <div className="button-container" onClick={add}>
+                <AddAnotherBtn sectionName="education"  />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Education;
