3fbd10fcf84626549180924fa1eae4f6c1b58a88
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-12-19 13:26:55 -0500

    Make EditableText class component a functional component

--- src/components/EditableText.js
+++ src/components/EditableText.js
@@ -1,35 +1,25 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import '../assets/css/EditableText.css';
 
-class EditableText extends Component {
-    constructor(props) {
-        super(props);
+const EditableText = ({element, textClass}) => {
 
-        this.onClickEdit = this.onClickEdit.bind(this);
-        this.onTextChange = this.onTextChange.bind(this);
-        this.onEditFocusOut = this.onEditFocusOut.bind(this);
-        this.handleKeyDown = this.handleKeyDown.bind(this);
-
-        this.state = {
-            editMode : false,
-            text : `Add ${this.props.textClass}`,
-        };
+    const [editMode, setEditMode]   = useState(false);
+    const [text, setText]           = useState(`Add ${textClass}`);
+    
+    const onClickEdit = () => {
+        setEditMode(true);
     }
 
-    onClickEdit() {
-        this.setState({ editMode : true} );
-    }
-
-    onTextChange(event) {
+    const onTextChange = (event) => {
         // if the input is empty...
         if (event.target.value === "") {
             // ...then set state text to default text
-            this.setState({ text : "Add "+event.target.className} );  
+            setText(`Add ${event.target.className}`);
         } else {
             // if the input is not empty...
 
             // ...then set state to user input
-            this.setState({ text : event.target.value} );   
+            setText(event.target.value);  
             
             // If the input is the name input...
             if (event.target.className === "name") {
@@ -40,77 +30,72 @@ class EditableText extends Component {
         }
     }
 
-    onEditFocusOut() {
-        this.setState({ editMode : false} );
+    const onEditFocusOut = () => {
+        setEditMode(false);
     }
 
-    handleKeyDown(e) {
-        if (e.key === "Enter") {this.onEditFocusOut()}
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {onEditFocusOut()}
+    }
+    
+    // If the element is a date, then modify the default/placeholder text
+    let placeholderText = text;
+    if (textClass === "start date" || textClass === "end date") {
+        placeholderText = textClass.charAt(0).toUpperCase() + textClass.slice(1);
     }
 
-    render() {
-        const {element, textClass} = this.props; 
-        const {editMode, text} = this.state; 
-        
-        // If the element is a date, then modify the default/placeholder text
-        let placeholderText = text;
-        if (textClass === "start date" || textClass === "end date") {
-            placeholderText = textClass.charAt(0).toUpperCase() + textClass.slice(1);
-        }
-
-        if (editMode) {
-            // If the text is the default text...
-            if (text === `Add ${textClass}`) {
-                // ... then use placeholder, not value
-                return (
-                    <textarea
-                    placeholder={placeholderText}
-                    autoFocus className={textClass} 
-                    onChange={this.onTextChange}
-                    onBlur={this.onEditFocusOut}
-                    onKeyDown={this.handleKeyDown}
-                    >
-                    </textarea>
-                );  
-            } else {
-                // If not default text... 
-                // ...then use value, not placeholder
-                return (
-                    <textarea
-                    value={text} 
-                    autoFocus className={textClass} 
-                    onChange={this.onTextChange}
-                    onBlur={this.onEditFocusOut}
-                    onKeyDown={this.handleKeyDown}
-                    >
-                    </textarea>
-                );  
-            }                                  
+    if (editMode) {
+        // If the text is the default text...
+        if (text === `Add ${textClass}`) {
+            // ... then use placeholder, not value
+            return (
+                <textarea
+                placeholder={placeholderText}
+                autoFocus className={textClass} 
+                onChange={onTextChange}
+                onBlur={onEditFocusOut}
+                onKeyDown={handleKeyDown}
+                >
+                </textarea>
+            );  
         } else {
+            // If not default text... 
+            // ...then use value, not placeholder
+            return (
+                <textarea
+                value={text} 
+                autoFocus className={textClass} 
+                onChange={onTextChange}
+                onBlur={onEditFocusOut}
+                onKeyDown={handleKeyDown}
+                >
+                </textarea>
+            );  
+        }                                  
+    } else {
 
-            let editedClass = "unedited";
-            // If the text is not the default text (it has been edited)...
-            if (text !== `Add ${textClass}`) {
-                // ...then change the editClass to change red text to black
-                editedClass = 'edited';
-            }
-
-            if (element === "p") {
-                if (text === `Add ${textClass}`) {
-                    return <p className={textClass+" "+editedClass} onClick={this.onClickEdit}>{placeholderText}</p>;
-                } else {
-                    return <p className={textClass+" "+editedClass} onClick={this.onClickEdit}>{text}</p>;
-                }
-            } else if (element === "h4") {
-                return <h4 className={textClass+" "+editedClass} onClick={this.onClickEdit}>{placeholderText}</h4>;
-            } else if (element === "h1") {
-                return <h1 className={textClass+" "+editedClass} onClick={this.onClickEdit}>{placeholderText}</h1>;
-            } else if (element === "em") {
-                return <em className={textClass+" "+editedClass} onClick={this.onClickEdit}>{placeholderText}</em>;
-            }
+        let editedClass = "unedited";
+        // If the text is not the default text (it has been edited)...
+        if (text !== `Add ${textClass}`) {
+            // ...then change the editClass to change red text to black
+            editedClass = 'edited';
         }
 
+        if (element === "p") {
+            if (text === `Add ${textClass}`) {
+                return <p className={textClass+" "+editedClass} onClick={onClickEdit}>{placeholderText}</p>;
+            } else {
+                return <p className={textClass+" "+editedClass} onClick={onClickEdit}>{text}</p>;
+            }
+        } else if (element === "h4") {
+            return <h4 className={textClass+" "+editedClass} onClick={onClickEdit}>{placeholderText}</h4>;
+        } else if (element === "h1") {
+            return <h1 className={textClass+" "+editedClass} onClick={onClickEdit}>{placeholderText}</h1>;
+        } else if (element === "em") {
+            return <em className={textClass+" "+editedClass} onClick={onClickEdit}>{placeholderText}</em>;
+        }
     }
+      
 }
 
 export default EditableText;
